1e4689fb0331d8fb86fe891baeea525418186107
Vincenzo Palleschi <vincenzo.palleschi@dcci.unipi.it>
2026-06-28 18:33:00 +0200

    Move template and identification dialogs to gui modules

---
+++ pylibs/gui/identification_dialogs.py
@@ -0,0 +1,7 @@
+"""Identification-related GUI dialogs for pyLIBS.
+
+No class was moved in this pass because the identification windows are tightly
+coupled to MainWindow database access, search logic, and template assignment
+callbacks.
+"""
+
---
+++ pylibs/gui/template_dialogs.py
@@ -0,0 +1,7 @@
+"""Template-related GUI dialogs for pyLIBS.
+
+No class was moved in this pass because the current template windows are
+tightly coupled to MainWindow template state, marker refresh, and template
+editing logic.
+"""
+
